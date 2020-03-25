9e889b0e3f773c6d2662cc2de6826d6a33d6790a
Kim <kim.yang@waveo.com>
2020-03-25 17:05:48 +0800

    add store & update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import { Button } from 'antd';
+
+import StoreProvider from './store';
+import TodoForm from './components/TodoForm';
+import List from './components/List';
+// import TodoListContext from './store/TodoListContext';
+
 function App() {
+  const [isModalShow, toggleModal] = useState(false);
+
+  // useEffect(() => {
+  //   const localTodos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+  // }, []);
+
   return (
     <div className="App">
-      <h1>TODO LIST</h1>
+      <StoreProvider>
+        <main style={{ width: '60%', margin: '0 auto' }}>
+          <h1>TODO LIST</h1>
+          <Button
+            onClick={() => toggleModal(true)}
+            size="large"
+            style={{ marginBottom: '16px' }}
+          >
+            ADD TODO
+          </Button>
+          <List />
+          <TodoForm isModalShow={isModalShow} onCancel={() => toggleModal(false)} />
+        </main>
+      </StoreProvider>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { useLocalStore } from 'mobx-react';
+import TodoListContext from './TodoListContext';
+
+const StoreProvider = ({ children }) => {
+  const store = useLocalStore(() => ({
+    todos: [],
+    showModal: false,
+    addTodo: (todo) => {
+      store.todos.unshift(todo);
+      localStorage.setItem('todos', JSON.stringify(store.todos));
+    },
+    changeStatus: (id) => {
+      store.todos = store.todos.map((todo) => {
+        if (todo.id === id) {
+          const status = todo.status === 'undone' ? 'done' : 'undone';
+          const date = new Date();
+          return { ...todo, status, date };
+        }
+        return todo;
+      });
+      localStorage.setItem('todos', JSON.stringify(store.todos));
+    },
+    editTodo: (id, data) => {
+      store.todos = store.todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, ...data };
+        }
+        return todo;
+      });
+      localStorage.setItem('todos', JSON.stringify(store.todos));
+    },
+    deleteTodo: (id) => {
+      store.todos = store.todos.filter((todo) => todo.id !== id);
+      localStorage.setItem('todos', JSON.stringify(store.todos));
+    },
+  }));
+
+  return (
+    <TodoListContext.Provider value={store}>{children}</TodoListContext.Provider>
+  );
+};
+
+StoreProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default StoreProvider;
