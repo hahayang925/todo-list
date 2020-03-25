ec84d2324392709f8704c1ddfcd90b7ddf5325c0
Kim <kim.yang@waveo.com>
2020-03-25 17:32:50 +0800

    project build

---
+++ docs/precache-manifest.ea57742ee7f22164c4695d237ffec46e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601ecfb7693b27f353dfe53c9d397a04",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "6a48ff3d02a9e8f8129d",
+    "url": "/todo-list/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "fe5412bcc02532147f89",
+    "url": "/todo-list/static/css/main.9eedbcbd.chunk.css"
+  },
+  {
+    "revision": "6a48ff3d02a9e8f8129d",
+    "url": "/todo-list/static/js/2.ce0a468a.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/todo-list/static/js/2.ce0a468a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5412bcc02532147f89",
+    "url": "/todo-list/static/js/main.4f834fa5.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
